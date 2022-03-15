f7d7d6fd74ee528959607ec2d05ec772ccfbc1e9
bstevenpo01 <91967732+bstevenpo01@users.noreply.github.com>
2022-03-15 02:52:49 +0100

    Crear modelo para alquiler

--- models/models.py
+++ models/models.py
@@ -16,3 +16,34 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+from odoo import models, fields, api
+
+class suscripcion(models.Model):
+    _name = 'alquiler.suscripcion'
+    _description = 'Define los atributos de la suscripcion'
+
+    # Atributos
+    nombreSus = fields.Char(string='Nombre suscripcion', required=True)
+
+class cliente(models.Model):
+    _name = 'alquiler.cliente'
+    _description = 'Define los atributos de un ciente'
+
+    # Atributos
+    dniCliente = fields.Char(string='DNI', required=True)
+    nombreCliente = fields.Char(string='Nombre y Apellidos', required=True)
+    fechaNacimiento = fields.Date(string='Fecha Nacimiento', required=True, default = fields.date.today())
+    direccionCliente = fields.Char(string='Direccion', required=True)
+    telefonoCliente = fields.Char(string='Telefono', required=True)
+
+
+class alquiler(models.Model):
+    _name = 'alquiler.alquiler'
+    _description = 'Define los atributos del alquiler'
+
+    #Atributos
+    nombreVideojuego = fields.Char(string='Nombre Videojuego', required=True)
+    tipoVideojuego = fields.Selection(string='Tipo de videojuego', Selection=[('d','Deportes')('a','Accion'),('c','Carreras'),('e','Estrategia')], help='Tipo de videojuego que esta alquilando')
+    descripcionVideojuego = fields.Text(string='Descripcion del videojuego')
+    

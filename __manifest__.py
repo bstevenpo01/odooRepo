77a067bb26f3e85d1a55e0c1f1ba98378be0f184
bstevenpo01 <91967732+bstevenpo01@users.noreply.github.com>
2022-03-17 05:42:18 +0100

    cambios

--- __manifest__.py
+++ __manifest__.py
@@ -28,6 +28,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'report/alquileres_clientes_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
